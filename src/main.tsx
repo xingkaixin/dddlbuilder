e1d72a25d0fa8fa3265fd546b1a0c629b5973e34
XingKaiXin <xingkaixin@gmail.com>
2026-02-01 20:39:19 +0800

    feat: add Vercel Analytics for usage tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App.tsx';
 
@@ -12,5 +13,6 @@ if (!rootElement) {
 createRoot(rootElement).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
