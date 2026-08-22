db0649c324a0671392c1f622b80e499dc117a954
XingKaiXin <xingkaixin@gmail.com>
2026-08-22 19:38:19 +0800

    refactor(web): remove obsolete state hooks

--- apps/web/src/hooks/index.ts
+++ apps/web/src/hooks/index.ts
@@ -4,12 +4,6 @@ export type { UseToastReturn } from './useToast';
 export { usePersistedState } from './usePersistedState';
 export type { UsePersistedStateReturn } from './usePersistedState';
 
-export { useTableData } from './useTableData';
-export type { UseTableDataReturn } from './useTableData';
-
-export { useIndexManagement } from './useIndexManagement';
-export type { UseIndexManagementReturn } from './useIndexManagement';
-
 export { useAuthManagement } from './useAuthManagement';
 export type { UseAuthManagementReturn } from './useAuthManagement';
 
